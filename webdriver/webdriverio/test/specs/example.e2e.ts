cbed96e01cf2eaaff77f39091cb1d51be6705f7a
Alex Rock Ancelet <pierstoval@gmail.com>
2022-03-03 15:02:15 +0100

    Fix test

--- webdriver/webdriverio/test/specs/example.e2e.ts
+++ webdriver/webdriverio/test/specs/example.e2e.ts
@@ -1,14 +1,8 @@
 describe("Hello Tauri", () => {
   it("should be cordial", async () => {
-    const header = await $("body h2");
+    const header = await $("body h1");
     const text = await header.getText();
-    expect(text).toMatch(/^[hH]ello/);
-  });
-
-  it("should be excited", async function () {
-    const header = await $("body h2");
-    const text = await header.getText();
-    expect(text).toMatch(/!$/);
+    expect(text).toMatch(/^Drag&drop test/);
   });
 });
 
